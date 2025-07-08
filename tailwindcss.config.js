d80c96b11cdc9b0a254bb350189e60d072318fa3
Asjad Hanif <asjad.hanif970@gmail.com>
2025-07-08 21:38:35 +0500

    delete tailwindcss

--- tailwindcss.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./index.html,script.js", "./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
